ce01f02c99be69065e30d525a5ee8d91d538d16d
LabelBlanc <LabelBlanc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-16 17:51:32 +0000

    Version 1.1 * New "Save and View" action * Major rewrite of the actions logic * Some bug fixes
    git-svn-id: https://plugins.svn.wordpress.org/improved-save-button/tags/1.1@1267552 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ improved-save-button.php
@@ -0,0 +1,151 @@
+<?php
+/*
+Plugin Name: Improved Save Button
+Description: Adds a new "Save" button to the Post Edit screen that saves the post and immediately redirects to one of the common screens: the Posts List, the New Post screen, the previous or next post's Edit screen or the post's frontend page.
+Author: Label Blanc
+Version: 1.1
+Author URI: http://www.labelblanc.ca
+Domain Path: /languages/
+Text Domain: lb-save-and-then
+*/
+
+/**
+ * Copyright 2015 Label Blanc (http://www.labelblanc.ca/)
+ *
+ * This file is part of the "Improved Save Button"
+ * Wordpress plugin.
+ *
+ * The "Improved Save Button" Wordpress plugin
+ * is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * All the PHP files of the plugin
+ * @var array
+ */
+$lib_files_to_include = array(
+	'class-lb-save-and-then-utils.php',
+	'class-lb-save-and-then-settings.php',
+	'class-lb-save-and-then-post-edit.php',
+	'class-lb-save-and-then-post-save.php',
+	'class-lb-save-and-then-messages.php',
+	'class-lb-save-and-then-actions.php',
+	'class-lb-save-and-then-action.php',
+);
+
+// Include all the PHP files of the plugin
+foreach ( $lib_files_to_include as $file_name ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'lib' . DIRECTORY_SEPARATOR . $file_name );
+}
+
+/**
+ * PHP files of the actions that come with the plugin
+ * @var array
+ */
+$actions_files_to_include = array(
+	'class-lb-save-and-then-action-new.php',
+	'class-lb-save-and-then-action-list.php',
+	'class-lb-save-and-then-action-view.php',
+	'class-lb-save-and-then-action-view-popup.php',
+	'class-lb-save-and-then-action-next.php',
+	'class-lb-save-and-then-action-previous.php',
+);
+
+// Include all the actions php files
+foreach ( $actions_files_to_include as $file_name ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'actions' . DIRECTORY_SEPARATOR . $file_name );
+}
+
+if( !class_exists( 'LB_Save_And_Then' ) ) {
+
+/**
+ * Main class. Mainly calls the setup function of other classes and
+ * define the list of 'actions'.
+ */
+class LB_Save_And_Then {
+
+	/**
+	 * Main entry point of the plugin. Calls the setup function
+	 * of the other classes.
+	 */
+	static function setup() {
+		LB_Save_And_Then_Settings::setup();
+		LB_Save_And_Then_Post_Edit::setup();
+		LB_Save_And_Then_Post_Save::setup();
+		LB_Save_And_Then_Messages::setup();
+		LB_Save_And_Then_Actions::setup();
+
+		// Priority 1, because the settings page is also on admin_init
+		// and uses translations
+		add_action( 'admin_init', array( get_called_class(), 'load_languages' ), 1 );
+		add_action( 'lbsat_load_actions', array( get_called_class(), 'load_default_actions' ) );
+	}
+
+	/**
+	 * Returns the localized name of the plugin
+	 * @return string
+	 */
+	static function get_localized_name() {
+		$plugin_data = get_plugin_data( __FILE__, false, true );
+		return $plugin_data['Name'];
+	}
+
+	/**
+	 * Called by the lbsat_load_actions filter. Loads all the
+	 * actions that come by default with the plugin.
+	 */
+	static function load_default_actions( $actions ) {
+		$default_actions_classes = array(
+			'LB_Save_And_Then_Action_New',
+			'LB_Save_And_Then_Action_List',
+			'LB_Save_And_Then_Action_Next',
+			'LB_Save_And_Then_Action_Previous',
+			'LB_Save_And_Then_Action_View',
+			'LB_Save_And_Then_Action_View_Popup',
+		);
+
+		foreach ( $default_actions_classes as $class_name ) {
+			$actions[] = new $class_name();
+		}
+
+		return $actions;
+	}
+
+	/**
+	 * Loads the language file for the admin. Must be called in the
+	 * 'admin_init' hook, since it uses get_plugin_data() and this
+	 * function is loaded once all admin files are included.
+	 */
+	static function load_languages() {
+		$plugin_data = get_plugin_data( __FILE__, false, true );
+		$path = dirname( LB_Save_And_Then_Utils::plugin_main_file_basename() );
+		$path .= $plugin_data['DomainPath'];
+		load_plugin_textdomain( $plugin_data['TextDomain'], false, $path );
+	}
+
+	/**
+	 * Returns the full path of the plugin's main file (this file).
+	 * Used in the utils
+	 *
+	 * @return string
+	 */
+	static function get_main_file_path() {
+		return __FILE__;
+	}
+
+} // end class
+
+} // end if( class_exists() )
+
+LB_Save_And_Then::setup();
---
+++ readme.txt
@@ -0,0 +1,76 @@
+=== Improved Save Button ===
+Contributors: LabelBlanc
+Tags: publish, save, close, list, edit, editing, return to list, close post, posts list, update, save and new, save and return, save and list, save and next, next post, save and previous, save and view, previous post, admin, administration, editor, multisite, custom post type, page, post
+Requires at least: 3.5
+Tested up to: 4.3
+Stable tag: 1.1
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Adds a button to the Edit Post screen which, in one click, saves the post then redirects you to the posts list, the New Post form, an adjacent post or the post's frontend page.
+
+== Description ==
+
+This plugin adds a new and improved "Save" button to the Post Edit screen that, in a single click, saves the current post and immediately redirects you to either:
+
+* The posts list (Save and List, a.k.a. "Save and Close"),
+* The New Post screen (Save and New),
+* The Post Edit screen of the previous or next post (Save and Previous/Next)
+* The post's page on the frontend (Save and View).
+
+This plugin saves you a lot of time when you have multiple posts, pages or custom posts to create or modify!
+
+Works with pages, posts and custom post types!
+
+Through the plugin's settings page, choose which actions are available and which one to use as the buttons' default action.
+
+Detail of the actions this new button allows:
+
+* **Save and List** (a.k.a. Save and Close): in a single click, save the current post and go back to the posts list.
+* **Save and New**: save the current post and go to the New Post screen.
+* **Save and Previous**: save the current post and go to the previous post's Edit screen.
+* **Save and Next**: save the current post and go to the next post's Edit screen.
+* **Save and View** (same or new window): save the current post and show the post's page on the frontend. Can show the post in the same window or in a new one.
+
+== Installation ==
+
+1. Download Improved Save Button.
+2. Upload the 'improved-save-button' directory to your '/wp-content/plugins/' directory, using your favorite method (ftp, sftp, scp, etc...)
+3. Activate Improved Save Button from your Plugins page.
+
+= Extra =
+Visit 'Settings > Improved Save Button' to adjust the configuration to your needs.
+
+== Screenshots ==
+
+1. The new button
+2. The down arrow reveals all the possible actions
+3. The settings page
+
+== Changelog ==
+
+= 1.1 =
+Release Date: October 17, 2015
+
+* New action: As requested, a "Save and View" action was added! This action shows the post's frontend page after the save. Two behaviors are available: show in the same window or show in a new window.
+* Enhancement: A title attribute on the 'Save and next/previous' action now shows the name of the next/previous post.
+* Enhancement: A big part of the code was rewritten to ease the addition of future new actions (no documentation yet, but you can develop plugins that add new actions, look in the code if interested!).
+* Some bug fixes, including one with required fields of ACF.
+
+= 1.0.2 =
+Release Date: August 13, 2015
+
+* Misc: Changed the title of the settings page from h2 to h1, like other settings pages in Wordpress 4.3
+
+= 1.0.1 =
+Release Date: April 30, 2015
+
+* Enhancement: Post Edit Spinner: Up to date with Wordpress 4.2 behavior.
+* Enhancement: Wordpress 4.2's new "removable query args" is now used.
+* Bug Fix: The "1 post updated" message was not always shown after a "Save and list".
+* Misc: Checked for add_query_arg() XSS attack possibility.
+
+= 1.0 =
+Release date: February 19, 2015
+
+Initial version
